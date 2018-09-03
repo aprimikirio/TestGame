5472a69968ce9fc02b9d0ec903c8fa1129fb7f68
APrimikirio <a.d.primikirio@gmail.com>
2018-09-03 22:15:32 +0300

    Аdd structures and objects

---
+++ TestGame/TestGame/Objects/GOSquare.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestGame.Structures;
+
+namespace TestGame.Objects
+{
+    class GOSquare : GameObject
+    {
+        public int Height
+        {
+            get { return height; }
+            set { this.height = value; this.width = value; }
+        }
+        public int Width
+        {
+            get { return height; }
+            set { this.height = value; this.width = value; }
+        }
+    }
+}
---
+++ TestGame/TestGame/Objects/GameObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestGame.Structures;
+
+namespace TestGame.Objects
+{
+    abstract class GameObject
+    {
+        public int height;
+        public int width;
+        public string name;
+        public Coords crd;
+    }
+}
