c009d2764e04b9ccf968887d7ccd10f4abc27215
APrimikirio <a.d.primikirio@gmail.com>
2018-09-20 13:10:40 +0300

    just another

---
+++ TestGame/TestGame/GameStart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using TestGame.Structures;
+using TestGame.Objects;
+
+namespace TestGame
+{
+    class GameStart
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var application = new App();
+            application.InitializeComponent();
+            application.Run();
+        }
+    }
+}
